a93fb9993a033f5f53f360250e2529b81bb49377
Dylanlj <dylanl-j@hotmail.ca>
2018-02-20 21:04:58 +0000

    added headers and footers, as well as url index and urls show

--- tinyurl/express_server.js
+++ tinyurl/express_server.js
@@ -13,18 +13,24 @@ app.get("/", (req, res) => {
   res.end("Hello!")
 })
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase)
-})
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// })
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase)
+// })
 
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars)
 })
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {shortURL: req.params.id};
+  res.render("urls_show", templateVars);
+});
  
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
