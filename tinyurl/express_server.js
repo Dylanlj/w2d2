e39c5c38529ce812b01aadab942c7c7e1d86f800
Dylanlj <dylanl-j@hotmail.ca>
2018-02-22 00:03:09 +0000

    finished steps 1-4 of user registration, created registration page, setu up branch, set up user object and created the registration handler

--- tinyurl/express_server.js
+++ tinyurl/express_server.js
@@ -13,6 +13,21 @@ let urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+
+
 app.get("/", (req, res) => {
   res.end("Hello!")
 })
@@ -23,19 +38,7 @@ app.get("/urls/new", (req,res) => {
   res.render("urls_new", templateVars);
 });
 
-//i believe this receives the req and res from urls_new.ejs as post request
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-//this effectively sends the client to urls_show.ejs
-  res.redirect(302, `http://localhost:8080/urls/${shortURL}`);
-})
 
-//removing a resource
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id]
-  res.redirect('http://localhost:8080/urls')
-})
 
 //updates a long URL
 app.post("/urls/:id", (req, res) => {
@@ -52,24 +55,43 @@ app.post("/login", (req, res) => {
   res.redirect('http://localhost:8080/urls')
 })
 
+//clears cookie on logout
 app.post("/logout", (req, res) => {
   res.clearCookie('username')
   res.redirect('http://localhost:8080/urls')
 })
 
+//urls page, index
 app.get("/urls", (req, res) => {
- let templateVars = {shortURL: req.params.id,
-                    longURL: urlDatabase[req.params.id],
+ let templateVars = {urls: urlDatabase,
                     username: req.cookies["username"] }
-  res.render("urls_show", templateVars);
+  res.render("urls_index", templateVars);
 })
 
-
-app.get("/urls", (req, res) => {
- let templateVars = {urls: urlDatabase}
-  res.render("urls_index", templateVars)
+//i believe this receives the req and res from urls_new.ejs as post request
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+//this effectively sends the client to urls_show.ejs
+  res.redirect(302, `http://localhost:8080/urls/${shortURL}`);
 })
 
+//registers new users
+app.get("/register", (req, res) => {
+  let templateVars = {username: req.cookies["username"]};
+  res.render('urls_register.ejs', templateVars)
+})
+
+app.post("/register", (req, res) => {
+// you may be able to do this more efficiently with a for loop, come back to it at the end
+  let randomString = generateRandomString()
+  users[randomString] = {};
+  users[randomString].id = randomString;  
+  users[randomString].email = req.body.email;
+  users[randomString]['password'] = req.body.password;
+  res.cookie('user_id', randomString);
+  res.redirect("http://localhost:8080/urls");
+})
 
 //provides page with long and short URLS
 app.get("/urls/:id", (req, res) => {
@@ -78,6 +100,15 @@ app.get("/urls/:id", (req, res) => {
                       username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
+
+//removing a url resource 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect('http://localhost:8080/urls')
+})
+
+
+
 
 //tells you if the server is running and listening for client requests
 app.listen(PORT, () => {
@@ -96,7 +127,6 @@ function generateRandomString(){
 }
 
 
-
 //redirects the client using the shortURLs longURL site
 app.get("/u/:shortURL", (req, res) => {
   if(!urlDatabase[req.params.shortURL]){
@@ -108,5 +138,5 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(302, urlDatabase[req.params.shortURL]);
 });
 
-
+// you're very inconsistent with '' and "", pick one stupid
 
